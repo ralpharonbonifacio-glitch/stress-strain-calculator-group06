f17e33b5876b3f2d60264fe8aec4f14fa439e78e
eivrardseraphimfranco-design <eivrardseraphim.franco@ust.edu.ph>
2026-09-05 12:09:45 +0000

    feat: add predefined material objects

---
+++ stress_calculator/database.py
@@ -0,0 +1,13 @@
+try:
+    from .properties import MaterialProperties 
+    from .material import Material, Metal, Plastic, Composite
+except ImportError:
+    from properties import MaterialProperties
+    from material import Material, Metal, Plastic, Composite
+
+STEEL_PROPERTIES = MaterialProperties( density=7850, yield_strength=250, typical_youngs_modulus=200 )
+ALUMINUM_PROPERTIES = MaterialProperties( density=2700, yield_strength=95, typical_youngs_modulus=69 )
+TITANIUM_PROPERTIES = MaterialProperties( density=4500, yield_strength=880, typical_youngs_modulus=114 )
+STEEL = Metal( name="Steel", properties=STEEL_PROPERTIES )
+ALUMINUM = Metal( name="Aluminum", properties=ALUMINUM_PROPERTIES )
+TITANIUM = Metal( name="Titanium", properties=TITANIUM_PROPERTIES )
